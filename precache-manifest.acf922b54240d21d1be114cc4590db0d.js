9226250c0cee9bb1b46c2566e592004868e97821
kapilmunjewar <kapilmunjewar88@gmail.com>
2019-10-18 07:04:53 -0700

    Updates

---
+++ precache-manifest.acf922b54240d21d1be114cc4590db0d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89c17ae738c91dedfa5ac3a81cd46e7",
+    "url": "/sampleapp/index.html"
+  },
+  {
+    "revision": "90673f1cccaf3a08307d",
+    "url": "/sampleapp/static/js/2.3366598f.chunk.js"
+  },
+  {
+    "revision": "de4761e168c00439173d",
+    "url": "/sampleapp/static/js/main.47d51f16.chunk.js"
+  },
+  {
+    "revision": "9b83aac9f732aa8f630d",
+    "url": "/sampleapp/static/js/runtime-main.8961fc4e.js"
+  }
+]);
